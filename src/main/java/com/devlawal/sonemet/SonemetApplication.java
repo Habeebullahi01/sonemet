00a60089103b1cdc515fe29fb5a3937f40aadb4e
Habeebullahi01 <lawalhabeebullahi008@gmail.com>
2025-07-30 11:10:08 +0100

    starter files

---
+++ src/main/java/com/devlawal/sonemet/SonemetApplication.java
@@ -0,0 +1,13 @@
+package com.devlawal.sonemet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonemetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonemetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlawal/sonemet/SonemetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlawal.sonemet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonemetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
